02e9429ee9aec704694f8be0341b192fe059a2dc
Tara Hughes <taraaelisabeth@Taras-Air.home>
2020-05-27 14:29:34 -0500

    added src and public

---
+++ src/components/main/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./style.css";
+import Navbar from "./navbar"
+
+export default function main() {
+    return <div>
+        {/* <Navbar />  */}
+    
+    <section class="hero is-info is-fullheight">
+        <div class="hero-head"></div>
+           
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <div class="column is-6 is-offset-3">
+                        <h1 class="title">
+                            Welcome.
+                        </h1>
+                        <h2 class="subtitle">
+                            It went like... is a podcast founded by two sisters, 
+                            Savannah and Tara. 
+                            They have a lot of crazy stories and want to hear 
+                            yours! 
+                        </h2>
+                        <div class="box">
+                        <form
+                action="https://formspree.io/taraelisabethhughes@gmail.com"
+                method="POST"
+            >
+                            <div class="field is-grouped">
+                                <p class="control is-expanded">
+                                    <input class="input" type="text" placeholder="Join my e-mail list" /> 
+                                <div>
+                                <p class="control">
+                                    <a class="button is-info">
+                                        Notify Me
+                                    </a>
+                                </p>
+                            </div>
+                            </p>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+    <script async type="text/javascript" src="../js/bulma.js"></script>
+</section>
+</div>
+
+}
